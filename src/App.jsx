0abf5ba7d49e0c91d84de395c228fdabdc385866
Ahmadghani27 <ghoniyusufghoniyusuf@gmail.com>
2025-12-10 12:55:36 +0700

    Fix Bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,21 @@
-import React, { Suspense, lazy } from 'react'; // Import Suspense & lazy
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
-import ScrollToTop from './components/ScrollToTop';
-
-// Ubah import biasa jadi lazy import
-const Home = lazy(() => import('./pages/Home'));
-const Projects = lazy(() => import('./pages/Projects'));
-const Profile = lazy(() => import('./pages/Profile'));
-const Experience = lazy(() => import('./pages/Experience'));
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Profile from './pages/Profile';
+import Experience from './pages/Experience';
+import './App.css';
 
 function App() {
   return (
-    <Router basename={import.meta.env.BASE_URL}>
-      <ScrollToTop />
+    <Router>
       <Layout>
-        {/* Bungkus Routes dengan Suspense */}
-        <Suspense fallback={<div className="min-h-screen bg-black text-white p-10">Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/experience" element={<Experience />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
-        </Suspense>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
       </Layout>
     </Router>
   );
