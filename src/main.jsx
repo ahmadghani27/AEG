32319c1adbe7400f0e644ae018918fe525c14225
Ahmadghani27 <ghoniyusufghoniyusuf@gmail.com>
2025-12-10 12:37:12 +0700

    Fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/AEG/">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
